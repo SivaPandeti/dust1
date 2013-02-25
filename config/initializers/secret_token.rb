0dd4543a4f517edfc9cdeeecb51feaab02e1eb98
Siva Pandeti <psivah@yahoo.com>
2013-02-24 22:41:03 -0500

    Initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dust1::Application.config.secret_token = '8a0cd7c998e2a4d54f418500603a8854c885f1815de45463f142621ddb8fbc4824eaf9898819cbc7cbb5dfd31cc2d7f9288cfe3fb32ce73622b8ec0edc2fd439'
